d167ec1bcea920d1492db3539edc1d2dff1293cd
BNSebastian <bucovala.sebastian@protonmail.com>
2024-02-12 21:45:16 +0200

    update

--- src/main/java/freevoice/core/config/SecurityConfiguration.java
+++ src/main/java/freevoice/core/config/SecurityConfiguration.java
@@ -28,90 +28,90 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
+        private final JwtAuthenticationFilter jwtAuthFilter;
+        private final AuthenticationProvider authenticationProvider;
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:4200",
-                "https://localhost:4200",
-                "http://frontend-service:4200",
-                "http://frontend-service",
-                "http://bucovala-sebastian.go.ro:4200",
-                "https://bucovala-sebastian.go.ro:4200",
-                "https://bucovala-sebastian.go.ro:5001",
-                "https://bucovala-sebastian.go.ro",
-                "http://172.21.0.2:4200",
-                "https://172.21.0.2:4200",
-                "http://192.168.1.131:4200",
-                "https://192.168.1.131:4200",
-                "http://192.168.1.132:4200"));
-        // configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                // configuration.setAllowedOrigins(Arrays.asList(
+                // "http://localhost:4200",
+                // "https://localhost:4200",
+                // "http://frontend-service:4200",
+                // "http://frontend-service",
+                // "http://bucovala-sebastian.go.ro:4200",
+                // "https://bucovala-sebastian.go.ro:4200",
+                // "https://bucovala-sebastian.go.ro:5001",
+                // "https://bucovala-sebastian.go.ro",
+                // "http://172.21.0.2:4200",
+                // "https://172.21.0.2:4200",
+                // "http://192.168.1.131:4200",
+                // "https://192.168.1.131:4200",
+                // "http://192.168.1.132:4200"));
+                configuration.setAllowedOrigins(List.of("*"));
+                configuration.setAllowedMethods(List.of("*"));
+                configuration.setAllowedHeaders(List.of("*"));
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/auth/**"))
-                    .permitAll();
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.authorizeHttpRequests(auth -> {
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/auth/**"))
+                                        .permitAll();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/user/**"))
-                    .permitAll();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/user/**"))
+                                        .permitAll();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/image/**"))
-                    .permitAll();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/image/**"))
+                                        .permitAll();
 
-            // TODO: ADJUST FOR VIDEO UPLOAD
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/video/**"))
-                    .permitAll();
+                        // TODO: ADJUST FOR VIDEO UPLOAD
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/video/**"))
+                                        .permitAll();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/videoComment/**"))
-                    .authenticated();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/videoComment/**"))
+                                        .authenticated();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/forumPost/**"))
-                    .permitAll();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/forumPost/**"))
+                                        .permitAll();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/forumComment/**"))
-                    .authenticated();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/forumComment/**"))
+                                        .authenticated();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/chat/**"))
-                    .authenticated();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/chat/**"))
+                                        .authenticated();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/paypal/**"))
-                    .permitAll();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/paypal/**"))
+                                        .permitAll();
 
-            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/article/**"))
-                    .permitAll();
+                        auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/article/**"))
+                                        .permitAll();
 
-            // auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/time/ADMIN")).hasRole("ADMIN");
+                        // auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/time/ADMIN")).hasRole("ADMIN");
 
-            // websocket
-            // auth.requestMatchers(AntPathRequestMatcher.antMatcher("/ws/**")).permitAll();
-        });
+                        // websocket
+                        // auth.requestMatchers(AntPathRequestMatcher.antMatcher("/ws/**")).permitAll();
+                });
 
-        http.csrf(AbstractHttpConfigurer::disable);
+                http.csrf(AbstractHttpConfigurer::disable);
 
-        http.cors(withDefaults()); // by default uses a bean named corsConfigurationSource
+                http.cors(withDefaults()); // by default uses a bean named corsConfigurationSource
 
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.authenticationProvider(authenticationProvider);
+                http.authenticationProvider(authenticationProvider);
 
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-        // // Adding a custom security filter for WebSocket connections
-        // http.headers(headers -> {
-        // headers.cacheControl(withDefaults()).disable();
-        // headers.frameOptions(withDefaults()).disable();
-        // });
+                // // Adding a custom security filter for WebSocket connections
+                // http.headers(headers -> {
+                // headers.cacheControl(withDefaults()).disable();
+                // headers.frameOptions(withDefaults()).disable();
+                // });
 
-        return http.build();
-    }
+                return http.build();
+        }
 }
